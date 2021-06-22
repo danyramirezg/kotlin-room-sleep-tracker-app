0febe1533aa892ff3ecdd20ea7a512e8cc759d58
danyramirezg <danyramirezg@gmail.com>
2021-06-22 13:51:03 -0500

    Adding the ViewModel

--- app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
+++ app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
@@ -16,25 +16,26 @@
 
 package com.example.android.trackmysleepquality
 
-//import androidx.room.Room
-//import androidx.test.ext.junit.runners.AndroidJUnit4
-//import androidx.test.platform.app.InstrumentationRegistry
-//import com.example.android.trackmysleepquality.database.SleepDatabase
-//import com.example.android.trackmysleepquality.database.SleepDatabaseDao
-//import com.example.android.trackmysleepquality.database.SleepNight
-//import org.junit.Assert.assertEquals
-//import org.junit.After
-//import org.junit.Before
-//import org.junit.Test
-//import org.junit.runner.RunWith
-//import java.io.IOException
-//
-///**
-// * This is not meant to be a full set of tests. For simplicity, most of your samples do not
-// * include tests. However, when building the Room, it is helpful to make sure it works before
-// * adding the UI.
-// */
-//
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.android.trackmysleepquality.database.SleepDatabase
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+/**
+ * This is not meant to be a full set of tests. For simplicity, most of your samples do not
+ * include tests. However, when building the Room, it is helpful to make sure it works before
+ * adding the UI.
+ */
+
 //@RunWith(AndroidJUnit4::class)
 //class SleepDatabaseTest {
 //
@@ -47,9 +48,9 @@ package com.example.android.trackmysleepquality
 //        // Using an in-memory database because the information stored here disappears when the
 //        // process is killed.
 //        db = Room.inMemoryDatabaseBuilder(context, SleepDatabase::class.java)
-//                // Allowing main thread queries, just for testing.
-//                .allowMainThreadQueries()
-//                .build()
+//            // Allowing main thread queries, just for testing.
+//            .allowMainThreadQueries()
+//            .build()
 //        sleepDao = db.sleepDatabaseDao
 //    }
 //
@@ -61,12 +62,11 @@ package com.example.android.trackmysleepquality
 //
 //    @Test
 //    @Throws(Exception::class)
-//    fun insertAndGetNight() {
+//    fun insertAndGetNight() = runBlocking {
 //        val night = SleepNight()
 //        sleepDao.insert(night)
 //        val tonight = sleepDao.getTonight()
 //        assertEquals(tonight?.sleepQuality, -1)
 //    }
 //}
-//
-//
+
